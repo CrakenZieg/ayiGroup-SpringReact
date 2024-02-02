eda551231c6bb34d584cae8d2e7cc1c508e3d3de
Francisco Ziegler <franciscoziegler@gmail.com>
2024-02-02 17:09:01 -0300

    por favor necesito codear cortala con los rayos y truenos, llove tranqui y no me cortes la luz

--- practicafinal/evaluacion1-front/src/components/login/Login.js
+++ practicafinal/evaluacion1-front/src/components/login/Login.js
@@ -5,9 +5,16 @@ export default function Login() {
 
     const { user, loginUser, logoutUser } = useAuth();
 
-  return (
-    {
-        if(user!=="")
-    }
+  return (    
+        user!==""?
+        <>
+            <h3>Ya estás autenticado como: {user}</h3>
+            <p>¿No sos vos? <a>Haz click aquí para cerrar sesión.</a></p>
+            <Link to={strSource}>Link externo a la receta</Link>
+        </>
+        :
+        <>
+        </>
+    
   )
 }
